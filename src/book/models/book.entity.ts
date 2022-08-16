4d30bb2cf38283df7daad5ba932ee260c0722bbd
satyawan auti <auti.satyavan3@gmail.com>
2022-08-16 18:26:49 +0530

    find by name and price working

--- src/book/models/book.entity.ts
+++ src/book/models/book.entity.ts
@@ -1,22 +1,39 @@
 import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
+import { IsNotEmpty, IsString, IsInt} from "class-validator";
+// import {IsNotEmpty} from "@nestjs/common";
+
 @Entity('book_detail')
 export class BookEntity {
     @PrimaryGeneratedColumn()
+    @IsInt()
     book_id: number;
 
     @Column({default: ''})
+    @IsNotEmpty()
+    @IsString()
     book_name: string;
 
     @Column()
+    @IsNotEmpty()
     author: string;
 
     @Column()
+    @IsNotEmpty()
+    @IsInt()
     price: number;
 
     @Column()
+    @IsNotEmpty()
+    @IsString()
     book_image: string;
 
     @Column()
+    @IsNotEmpty()
+    @IsString()
     book_isbn: string;
 }
+
+// function IsNotEmpty() {
+//     throw new Error("Function not implemented.");
+// }
