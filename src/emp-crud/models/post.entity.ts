24e63abc36bd41a5e3a3e4c830a910668c705155
vaibhav-rahinj <rahinjvaibhav123@gmail.com>
2022-08-17 18:29:00 +0530

    Resolve conflict

--- src/emp-crud/models/post.entity.ts
+++ src/emp-crud/models/post.entity.ts
@@ -1,21 +1,9 @@
-// import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
-
-// @Entity('emp_crud')
-// export class EmpCrudEntity {
-//   @PrimaryGeneratedColumn()
-//   id: number;
-
-//   @Column({default:''})
-//   body:string;
-
-//   @Column({type:'timestamp', default:()=>'CURRENT_TIMESTAMP'})
-//   createdAt:Date;
-
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 @Entity('employee')
 export class EmpCrudEntity {
   @PrimaryGeneratedColumn('uuid')
   id: number;
+
   @Column({ default: '' })
   empFname: string;
   @Column()
