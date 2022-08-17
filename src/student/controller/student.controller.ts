9ba606817fe1d65fabdfffde6482cbd0a3039f27
madhuri-phatake <phatakemadhuri1999@gmail.com>
2022-08-17 12:47:34 +0530

    added student module

---
+++ src/student/controller/student.controller.ts
@@ -0,0 +1,50 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { DeleteResult, UpdateResult } from 'typeorm';
+import { StudentInterface } from '../dto/dto.interface';
+import { StudentService } from '../services/student.service';
+
+@Controller('student')
+export class StudentController {
+  constructor(private studentService: StudentService) {}
+  @Post() create(
+    @Body() studentPost: StudentInterface,
+  ): Observable<StudentInterface> {
+    return this.studentService.createPost(studentPost);
+  }
+
+  @Get() findAll(): Observable<StudentInterface[]> {
+    return this.studentService.findAllPosts();
+  }
+
+  @Put(':id') update(
+    @Param('id') id: number,
+    @Body() studentInterface: StudentInterface,
+  ): Observable<UpdateResult> {
+    return this.studentService.updatePutPost(id, studentInterface);
+  }
+
+  @Patch(':id') updatePatch(
+    @Param('id') id: number,
+    @Body() studentPost: StudentInterface,
+  ): Observable<UpdateResult> {
+    return this.studentService.updatePatchPost(id, studentPost);
+  }
+
+  @Delete(':id') delete(@Param('id') id: number): Observable<DeleteResult> {
+    return this.studentService.deletePost(id);
+  }
+  @Get(':id')
+  getStudentById(@Param('id') id: number): Observable<StudentInterface> {
+    return this.studentService.getstudentById(id);
+  }
+}
