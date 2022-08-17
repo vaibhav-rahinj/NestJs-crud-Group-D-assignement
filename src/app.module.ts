611e2b6e63aa1a61ffc3c2d7f8320d8632489c5f
madhuri-phatake <phatakemadhuri1999@gmail.com>
2022-08-17 13:03:05 +0530

    changes in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { BookModule } from './book/book.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
+import { StudentModule } from './student/student.module';
+import { StudentController } from './student/controller/student.controller';
 
 
 @Module({
@@ -23,9 +25,10 @@ import { UserModule } from './user/user.module';
       synchronize: true,
     }),
       BookModule,
-      UserModule
+      UserModule,
+      StudentModule
     ],
-  controllers: [AppController, UserController],
+  controllers: [AppController, UserController,StudentController],
   providers: [AppService],
 })
 export class AppModule {}
