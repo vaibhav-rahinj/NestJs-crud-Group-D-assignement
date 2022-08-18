5ed707ef7a48e0e6eed6e29cf5a99eead8e24429
satyawan auti <auti.satyavan3@gmail.com>
2022-08-18 10:30:10 +0530

    resolve branch issue

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.useGlobalPipes(new ValidationPipe({
+//     disableErrorMessages:false,
+//     whitelist:true,
+//     forbidNonWhitelisted:true
+//   }))
+//   // app.setGlobalPrefix('book');
+//   await app.listen(3003);
+// }
+// bootstrap();
+
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    disableErrorMessages:false,
-    whitelist:true,
-    forbidNonWhitelisted:true
-  }))
-  // app.setGlobalPrefix('book');
-  await app.listen(3003);
+  const config = new DocumentBuilder()
+    .setTitle('Book Management')
+    .setDescription('Add show Edit Delete -> Book data')
+    .setVersion('1.0')
+    .addTag('Group D Assignment')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3000);
 }
 bootstrap();
