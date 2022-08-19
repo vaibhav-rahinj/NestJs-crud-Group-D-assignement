8e7f4aadafda5ae3ababaaff578c3e376e781458
madhuri-phatake <phatakemadhuri1999@gmail.com>
2022-08-19 15:10:14 +0530

    feat:image code is added

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
@@ -12,6 +13,7 @@ async function bootstrap() {
     .addTag('cats')
     .build();
   const document = SwaggerModule.createDocument(app, config);
+  app.useGlobalPipes(new ValidationPipe());
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
