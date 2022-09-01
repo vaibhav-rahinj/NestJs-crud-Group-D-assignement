1d39a28b87c7e86071205bfc6b8ddfbe8ec87257
vaibhav-rahinj <rahinjvaibhav123@gmail.com>
2022-09-01 15:02:27 +0530

    feat/many to many relation in nest js

--- src/main.ts
+++ src/main.ts
@@ -65,12 +65,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-        disableErrorMessages:false,
-        enableDebugMessages	: true,
-        whitelist:true,
-        forbidNonWhitelisted:true
-      }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      disableErrorMessages: false,
+      enableDebugMessages: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   const config = new DocumentBuilder()
     .setTitle('Book Management')
     .setDescription('Add show Edit Delete -> Book data')
@@ -81,6 +83,6 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.enableCors();
-  await app.listen(3006);
+  await app.listen(4000);
 }
 bootstrap();
